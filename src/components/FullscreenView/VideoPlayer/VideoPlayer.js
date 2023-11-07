0362011307824f81ba327bb1692ac5a7e4ac6d0e
Jah <jahsias.white@gmail.com>
2023-11-06 20:17:21 -0600

    Fixed an issue where the video wouldnt reset if a new audio effect was added

--- src/components/FullscreenView/VideoPlayer/VideoPlayer.js
+++ src/components/FullscreenView/VideoPlayer/VideoPlayer.js
@@ -3,18 +3,27 @@ import React, { useEffect, useRef } from 'react';
 import { useAudioPlayer } from '../../../AudioController/AudioContext';
 
 function VideoPlayer({ songFile, song }) {
-  const { videoTime, currentSpeed, isPlaying } = useAudioPlayer();
+  const { videoTime, currentSpeed, isPlaying, loadingQueue } = useAudioPlayer();
 
   const videoRef = useRef(null);
 
   useEffect(() => {
     // Whenever the song or song ID changes, update the video source
-    videoRef.current.src = songFile;
+    // videoRef.current.src = songFile;
 
     videoRef.current.currentTime = song.currentTime; // Set the video's currentTime
 
-    videoRef.current.playbackRate = currentSpeed; // TODO ! Takes time to render, setCurrentSpeed fires too fast meaning song.currentTime isn't updated properly
-  }, [songFile, videoTime, currentSpeed]); // TODO: Better way to do this? Need this component to update but would rather not have an extra useState
+    // videoRef.current.playbackRate = currentSpeed; // TODO ! Takes time to render, setCurrentSpeed fires too fast meaning song.currentTime isn't updated properly
+  }, [videoTime]); // TODO: Better way to do this? Need this component to update but would rather not have an extra useState
+
+  useEffect(() => {
+    // If loading is done, update the video
+    if (loadingQueue.length === 0) {
+      videoRef.current.src = songFile;
+      videoRef.current.currentTime = song.currentTime; // Set the video's currentTime
+      videoRef.current.playbackRate = currentSpeed; // TODO ! Takes time to render, setCurrentSpeed fires too fast meaning song.currentTime isn't updated properly
+    }
+  }, [loadingQueue]);
 
   // ? Will the current time slowly lose sync? If we update currentTime everytime though, there is a visual stutter
   useEffect(() => {
